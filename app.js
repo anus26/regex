e746f6829a0481f30ff380a7b2f3f47be8230e44
anas-raza~ <anusraza268@gmail.com>
2024-05-22 22:26:37 -0700

    /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/

--- app.js
+++ app.js
@@ -2,14 +2,22 @@
 
 const form=document.querySelector('#form')
 const username=document.querySelector('#username')
-const passward=document.querySelector('#passward')
+const password=document.querySelector('#password')
 const cnic=document.querySelector('#cnic')
 const email=document.querySelector('#email')
 
 
+ let emailRegex=/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/
 
+ let cnicRegex= /^42\d{11}$/;
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault()
-    console.log(username.value);
+    // console.log(username.value);
+    // console.log(cnic.value);
+    // console.log(email.value);
+    // console.log(password.value);
+    
+console.log(emailRegex.test(email.value));
+    console.log(cnicRegex.test(email.value));
 })
