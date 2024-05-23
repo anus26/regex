5392d75dc31e53c23941a9992d7cf355aa1735cc
anas-raza~ <anusraza268@gmail.com>
2024-05-23 04:56:38 -0700

    modified

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 
-
-const form=document.querySelector('#form')
-const username=document.querySelector('#username')
-const password=document.querySelector('#password')
-const cnic=document.querySelector('#cnic')
-const email=document.querySelector('#email')
-
-
- let emailRegex=/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/
-
- let cnicRegex= /^42\d{11}$/;
+ const emailRegex=/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/
+ const usernameRegex = /^[a-zA-Z0-9]{4,5}$/;
+const cnicRegex= /^\d{11}$/;
+const passwordRegex = /^(?=.*\d)[a-zA-Z0-9]{8,}$/;
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault()
+    
+    
+    const form=document.querySelector('#form')
+    const username=document.querySelector('#username')
+    const password=document.querySelector('#password')
+    const cnic=document.querySelector('#cnic')
+    const email=document.querySelector('#email')
     // console.log(username.value);
     // console.log(cnic.value);
-    // console.log(email.value);
+    // console.log(email.value);`
     // console.log(password.value);
     
-console.log(emailRegex.test(email.value));
-    console.log(cnicRegex.test(email.value));
+    console.log(emailRegex.test(email.value));
+    console.log(cnicRegex.test(cnic.value));
+    console.log(usernameRegex.test(username.value));
+    console.log(passwordRegex.test(password.value));
+
 })
